74b7905a8e10f5ca7d67d78e838c35227fb9b780
limit07065 <raymond07065@gmail.com>
2016-11-13 18:28:14 +0800

    created view.aspx

---
+++ SV/View.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SV_View : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //string sesisem = Session["acadUserSs"].ToString();
+            showProfile();
+            //lblLoggedInUser.Text = Session["acadUserNm"].ToString();
+        }
+
+    }
+
+    protected void showProfile()
+    {
+        imgPhoto.InnerHtml = "<img src=\"../Styles/images/nophoto.png\" class=\"img-profile\" width=\"100\" alt=\"profileimage\" />";
+        lblName.Text = Session["acadUserNm"].ToString();
+        lblProgramme.Text = Session["acadUserPr"].ToString();
+        lblFaculty.Text = Session["acadUserFn"].ToString();
+        lblMatric.Text = Session["acadUserMt"].ToString();
+        lblBilSemester.Text = Session["acadUserBs"].ToString() + " / " + Session["acadUserNs"].ToString();
+        lblAA.Text = Session["acadUserSv"].ToString();
+        lblToS.Text = Session["acadUserTs"].ToString() + " (Full Time)";
+        lblEmail.Text = Session["acadUserEm"].ToString();
+        lblPhone.Text = Session["acadUserTl"].ToString();
+    }
+}
